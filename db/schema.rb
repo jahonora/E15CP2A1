ae4758fcee0411e5f01a718f11f1b7da1820fea4
Jose Antonio H <jahonora@uc.cl>
2018-06-26 20:09:03 -0400

    changing sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20180625013111) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "histories", force: :cascade do |t|
     t.string "title"
     t.string "picture"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_histories_on_user_id"
   end
 
@@ -37,4 +40,5 @@ ActiveRecord::Schema.define(version: 20180625013111) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "histories", "users"
 end
